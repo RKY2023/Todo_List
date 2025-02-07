a4ff6c8298d6d2c4452224809bf809634c4b3ed9
RKY <xraj2023@gmail.com>
2025-02-07 11:17:41 +0530

    todo app

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Button } from "@heroui/button"
+import TodoApp from "./todo";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,28 +18,7 @@ export default function Home() {
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        Todo-List App
-        <Button> sdss</Button>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="#"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Today
-          
-        </a>
-      </footer>
+      <TodoApp />
     </div>
   );
 }
