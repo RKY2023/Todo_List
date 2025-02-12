c1337387be5fa33a3eaae8484c8ca56cb08c18e1
raj <xraj2023@gmail.com>
2025-02-12 17:13:46 +0530

    added

--- server/index.js
+++ server/index.js
@@ -1,9 +1,30 @@
+const path = require("path");
 const express = require("express");
+
+require('dotenv').config();
+
+// const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const todoRoutes = require("./routes/todoRoutes");
 const app = express();
+
+// const mongodbC = require('./test');
+// mongodbC.run();
 
 const port = 3001;
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.use(bodyParser.json());
+
+// models
+
+// routes
+app.use("/todos", todoRoutes);
+
+const WelcomeToExpress = (req, res) => {
+    res.send("Express on Vercel");
+  };
+
+app.get("/", WelcomeToExpress);
 
 app.listen(port, () => console.log(`Server ready on port ${port}.`));
 
